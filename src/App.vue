88fbcd7367d5518e67f32231430d78205ce28343
LucaB98 <lucabellan98@gmail.com>
2024-07-23 22:08:16 +0200

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppForm from "./components/AppForm.vue";
+import AppHeader from "./components/AppHeader.vue";
+export default {
+  name: 'AppVue',
+  components: { AppForm, AppHeader },
+};
+</script>
+
+<template>
+  <AppHeader />
+  <main id="main" class="mt-5">
+    <AppForm />
+  </main>
+
+</template>
+
+<style scoped></style>
