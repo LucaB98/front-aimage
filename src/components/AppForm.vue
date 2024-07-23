567c6fc0a02d6b1c6e8d3a3ac16b50966fe2d429
LucaB98 <lucabellan98@gmail.com>
2024-07-23 22:10:53 +0200

    add comments

--- src/components/AppForm.vue
+++ src/components/AppForm.vue
@@ -5,10 +5,12 @@ export default {
 </script>
 
 <template>
+    <!-- Form -->
     <div class="container">
         <div class="row row-cols-sm-1 row-cols-md-2">
-            <div class="col">
 
+            <div class="col">
+                <!-- Nome -->
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Nome</label>
                     <input type="text" class="form-control" id="exampleFormControlInput1"
@@ -16,7 +18,7 @@ export default {
                 </div>
             </div>
             <div class="col">
-
+                <!-- cognome -->
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Cognome</label>
                     <input type="text" class="form-control" id="exampleFormControlInput1"
@@ -24,7 +26,7 @@ export default {
                 </div>
             </div>
             <div class="col">
-
+                <!-- email -->
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Email</label>
                     <input type="email" class="form-control" id="exampleFormControlInput1"
@@ -32,7 +34,7 @@ export default {
                 </div>
             </div>
             <div class="col">
-
+                <!-- Telefono -->
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Telefono</label>
                     <input type="number" class="form-control" id="exampleFormControlInput1"
@@ -40,8 +42,7 @@ export default {
                 </div>
             </div>
             <div class="col">
-
-
+                <!-- Corso -->
                 <div class="mb-3">
                     <select class="form-select" aria-label="Default select example">
                         <option selected>Open this select menu</option>
