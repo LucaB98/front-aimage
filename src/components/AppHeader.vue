d2f745a97d48084db0e81f8bb7b843df048473cb
LucaB98 <lucabellan98@gmail.com>
2024-07-23 22:26:23 +0200

    add logo navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,7 +10,7 @@ export default {
     <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand ms-2" href="#"><img src="../img/aimage-logo.webp" alt="logo" width="100px"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
