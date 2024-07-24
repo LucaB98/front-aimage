eeb77b1c8e9e53ac996b0840e3f2125dd691421e
LucaB98 <lucabellan98@gmail.com>
2024-07-24 16:34:25 +0200

    import font

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// Supports weights 100-900
+import '@fontsource/montserrat';
 
 
 import { createApp } from 'vue'
